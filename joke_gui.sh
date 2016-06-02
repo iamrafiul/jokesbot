c126c19289b686fc4f4b86772d11591c50c56344
Rafiul Sabbir <iamrafiul@gmail.com>
2016-06-02 12:52:10 +0300

    Commit with fixing TUI and  server implementation problems

--- joke_gui.sh
+++ joke_gui.sh
@@ -1,6 +1,53 @@
 #echo "Enter Country Name: "
 #read sign
 
+JOKEBOT=jokebot
+HOST=localhost
+PORT=1234
+
+
+if [ -r ~/.jokes.conf ]
+then
+	. ~/.jokes.conf
+elif [ -r /etc/jokes.conf ]
+then
+	. /etc/jokes.conf
+fi
+
+help () {
+        echo "Usage: $0 [ host [port] | --help ]"
+}
+
+end () {
+	exit
+}
+
+if [ $# -eq 1 ]
+then
+        if [ $1 -eq '--help' ]
+        then
+                help
+                exit -1
+        else
+                HOST=$1
+        fi
+elif [ $# -eq 2 ]
+then
+        PORT=$2
+fi
+
+if [ $# -ge 1 -o `basename $0` = 'joke_gui' ]
+then
+	if [ $HOST -a $PORT ]
+	then
+	        JOKEBOT="nc $HOST $PORT"
+	else
+		echo "You must set HOST and PORT"
+		exit -2
+	fi
+fi
+
+
 DIALOG=gdialog || DIALOG=dialog
 
 while true
